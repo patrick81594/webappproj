ffe653fc4b984660c421ba9e0a0f2436c0a47ec4
patrick81594 <patrick94158@gmail.com>
2018-04-20 17:11:31 -0400

    Add project files.

---
+++ testproj/Data/AuthRepos.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using testproj.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace testproj.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly DataContext _context;
+
+        public AuthRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task<User> Login(string userName, string password)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.UserName == userName);
+
+            return (user == null || !PasswordHashVerified(password, user.PasswordHash, user.PasswordSalt)) ? null : user;
+        }
+
+        private bool PasswordHashVerified(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            // Hash password and compare with PasswordHash stored in database
+
+            // Temporary return value for testing
+            return true;
+        }
+
+        public async Task<User> Register(string userName, string password)
+        {
+            // Hash the password using SHA512 with random key (salt)
+            var hash = new HMACSHA512();
+            var computedHash = hash.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+
+            var newUser = new User { UserName = userName };
+            newUser.PasswordHash = computedHash;
+            newUser.PasswordSalt = hash.Key;
+
+            await _context.Users.AddAsync(newUser);
+            await _context.SaveChangesAsync();
+            return newUser;
+        }
+    }
+}
---
+++ testproj/Data/IAuthRepos.cs
@@ -0,0 +1,14 @@
+﻿using testproj.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace testproj.Data
+{
+    public interface IAuthRepository
+    {
+        Task<User> Register(string user, string password);
+        Task<User> Login(string userName, string password);
+    }
+}
